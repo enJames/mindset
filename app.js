65031f3b604162c509d08443158781db2e3206fd
James <spec.en.james@gmail.com>
2018-02-14 20:17:36 +0100

    first commit (though wrongly made, pushed too many things at once)

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+const express = require('express');
+const mainController = require('./controllers/mainController');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('static-files'));
+
+mainController(app);
+
+app.listen(1300, (err) => {
+    console.log('Now live at 1300');
+});
