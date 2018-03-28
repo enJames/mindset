1e8adfca50e425c4485b532930e8e44956c7ee7f
Enejo James <spec.en.james@gmail.com>
2018-03-28 21:41:22 +0100

    Add feedback content

---
+++ server/models/Feedback.js
@@ -0,0 +1,9 @@
+const Feedback = {
+    feedback_8_16: "You firmly believe that your talents, skills, and abilities are set traits. These things can't be changed very much. If you can't perform really well and look good on a test or project you would rather just not do it. You think that smart and talented people don't have to work very hard to be good.",
+    feedback_17_24: "You think that your skills and intelligence probably don't change much. You like situations where you perform well, are less likely to make mistakes, and don't have to put in too much effort. You believe that learning and getting better at things should be relatively easy.",
+    feedback_25_32: "You're not too sure whether or not you can change your skills and intelligence. Your grades and performances are important to you and so is learning. You're not the biggest fan of putting in too much effort though.",
+    feedback_33_40: "You believe that you can develop your skills and intelligence. You really care about learning and don't mind having to put in some effort to make it happen. Performing well matters to you but you think that learning is actually more important than always scoring well and looking good.",
+    feedback_41_48: "You totally believe that you can grow and improve your skills and intelligence. You love challenges and know that the best way to learn is by working really hard. You don't mind making mistakes or looking bad in order to get better."
+};
+
+export default Feedback;
